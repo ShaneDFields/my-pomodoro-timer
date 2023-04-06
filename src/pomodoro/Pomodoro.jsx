746b8b94a31ddeb1906bc34820c9b84d2cfbc2a7
Shane Fields <82911768+ShaneDFields@users.noreply.github.com>
2023-04-05 21:53:26 -0400

    update UI

--- src/pomodoro/Pomodoro.jsx
+++ src/pomodoro/Pomodoro.jsx
@@ -2,16 +2,7 @@ import React, { useState } from "react";
 import classNames from "../utils/class-names";
 import useInterval from "../utils/useInterval";
 import "./pomodoro.css";
-// These functions are defined outside of the component to insure they do not have access to state
-// and are, therefore more likely to be pure.
 
-/**
- * Update the session state with new state after each tick of the interval.
- * @param prevState
- *  the previous session state
- * @returns
- *  new session state with timing information updated.
- */
 function nextTick(prevState) {
   const timeRemaining = Math.max(0, prevState.timeRemaining - 1);
   return {
@@ -24,19 +15,7 @@ function fmtMSS(s) {
   return (s - (s %= 60)) / 60 + (9 < s ? ":" : ":0") + s;
 }
 
-/**
- * Higher order function that returns a function to update the session state with the next session type upon timeout.
- * @param focusDuration
- *    the current focus duration
- * @param breakDuration
- *    the current break duration
- * @returns
- *  function to update the session state.
- */
 function nextSession(focusDuration, breakDuration) {
-  /**
-   * State function to transition the current session type to the next session. e.g. On Break -> Focusing or Focusing -> On Break
-   */
   return (currentSession) => {
     if (currentSession.label === "Focusing") {
       return {
@@ -52,23 +31,15 @@ function nextSession(focusDuration, breakDuration) {
 }
 
 function Pomodoro() {
-  // Timer starts out paused
   const [isTimerRunning, setIsTimerRunning] = useState(false);
-  // The current session - null where there is no session running
   const [session, setSession] = useState(null);
   const [elapsed, setElapsed] = useState(0);
 
-  // ToDo: Allow the user to adjust the focus and break duration.
   const [focusDuration, setFocusDuration] = useState(25);
   const [breakDuration, setBreakDuration] = useState(5);
   const [aria, setAria] = useState(0);
   const [breakLeft, setBreakLeft] = useState(0);
 
-  /**
-   * Custom hook that invokes the callback function every second
-   *
-   * NOTE: You will not need to make changes to the callback function
-   */
   useInterval(
     () => {
       setBreakLeft(breakLeft + 1);
